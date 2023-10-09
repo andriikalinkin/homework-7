be464c72c3936173714eff6627976a6c4e95fce5
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-10-09 23:04:57 +0200

    Add db.sqlite3

--- school/views.py
+++ school/views.py
@@ -5,20 +5,20 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Homework-6 index page")
+    return HttpResponse("<h1>Homework-6 index page</h1>")
 
 
 def teacher(request):
-    return HttpResponse("\"teacher/\" page")
+    return HttpResponse("<h1>\"teacher/\" page</h1>")
 
 
 def teachers(request):
-    return HttpResponse("\"teachers/\" page")
+    return HttpResponse("<h1>\"teachers/\" page</h1>")
 
 
 def group(request):
-    return HttpResponse("\"group/\" page")
+    return HttpResponse("<h1>\"group/\" page</h1>")
 
 
 def groups(request):
-    return HttpResponse("\"groups/\" page")
+    return HttpResponse("<h1>\"groups/\" page</h1>")
