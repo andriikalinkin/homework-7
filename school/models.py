045b568239d106fc37989ca433b91f49ee920a89
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-10-10 21:25:47 +0200

    Create model. Make migration. Migrate.

--- school/models.py
+++ school/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=50, null=False)
+    last_name = models.CharField(max_length=50, null=False)
+    birth_date = models.DateField(null=False)
+    subject = models.CharField(max_length=255)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    curator = models.ForeignKey(Teacher, on_delete=models.CASCADE)
