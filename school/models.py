1ecfaf82909c230be8298671f205ec0573b41c4e
Andrii Kalinkin <andriikalinkin@gmail.com>
2023-10-11 22:44:09 +0200

    Edit models.py

--- school/models.py
+++ school/models.py
@@ -6,9 +6,9 @@ class Teacher(models.Model):
     first_name = models.CharField(max_length=50, null=False)
     last_name = models.CharField(max_length=50, null=False)
     birthdate = models.DateField(null=False)
-    subject = models.CharField(max_length=255)
+    subject = models.CharField(max_length=50)
 
 
 class Group(models.Model):
-    name = models.CharField(max_length=255, null=False)
+    name = models.CharField(max_length=50, null=False)
     curator = models.ForeignKey(Teacher, on_delete=models.CASCADE)
